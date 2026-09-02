a5ed1327cdcb859278cc6b595a6794f2c9929fdd
FlyingCat500 <mauro.torrengo@gmail.com>
2026-09-02 13:15:46 +0200

    Update start-llama-servers.sh
    restored --no-mmap

--- start-llama-servers.sh
+++ start-llama-servers.sh
@@ -13,10 +13,10 @@ LLAMA_SERVER_EXEC=${LLAMACPP_SERVER_BIN:-llama-server}
 # We use nohup to ensure the processes don't die if you close the terminal
 echo "Starting Model Server using: $LLAMA_SERVER_EXEC"
 #nohup "$LLAMA_SERVER_EXEC" -m "$LLAMACPP_PATH_MODEL" -c "$LLAMACPP_MODEL_CONTEXT_SIZE" -ngl "$LLAMACPP_MODEL_NGL" -fa on --port "$LLAMA_CPP_MODEL_PORT" -np "$LLAMACPP_MODEL_NUM_PARALLEL_SLOTS" --no-mmap --reasoning "$LLAMACPP_MODEL_REASONING" --chat-template-kwargs "{\"enable_thinking\": ${LLAMACPP_MODEL_THINKING:-false}}" > $HOME/thesis-project/sample-project/logs/model.log 2>&1 &
-nohup "$LLAMA_SERVER_EXEC" -m "$LLAMACPP_PATH_MODEL" -c "$LLAMACPP_MODEL_CONTEXT_SIZE" -ngl "$LLAMACPP_MODEL_NGL" -fa on --port "$LLAMA_CPP_MODEL_PORT" -np "$LLAMACPP_MODEL_NUM_PARALLEL_SLOTS" --load-mode mmap > $HOME/thesis-project/sample-project/logs/model.log 2>&1 &
+nohup "$LLAMA_SERVER_EXEC" -m "$LLAMACPP_PATH_MODEL" -c "$LLAMACPP_MODEL_CONTEXT_SIZE" -ngl "$LLAMACPP_MODEL_NGL" -fa on --port "$LLAMA_CPP_MODEL_PORT" -np "$LLAMACPP_MODEL_NUM_PARALLEL_SLOTS" --no-mmap > $HOME/thesis-project/sample-project/logs/model.log 2>&1 &
 
 echo "Starting Embedding Server using: $LLAMA_SERVER_EXEC"
-nohup "$LLAMA_SERVER_EXEC" -m "$LLAMACPP_PATH_EMBEDDINGS_MODEL" -c "$LLAMACPP_EMBED_CONTEXT_SIZE" -ngl "$LLAMACPP_EMBED_NGL" --port "$LLAMA_CPP_EMBED_PORT" --embedding --load-mode mmap --pooling "$LLAMACPP_EMBED_POOLING"  > $HOME/thesis-project/sample-project/logs/embedding.log 2>&1 &
+nohup "$LLAMA_SERVER_EXEC" -m "$LLAMACPP_PATH_EMBEDDINGS_MODEL" -c "$LLAMACPP_EMBED_CONTEXT_SIZE" -ngl "$LLAMACPP_EMBED_NGL" --port "$LLAMA_CPP_EMBED_PORT" --embedding --no-mmap --pooling "$LLAMACPP_EMBED_POOLING"  > $HOME/thesis-project/sample-project/logs/embedding.log 2>&1 &
 
 echo "Servers started in the background."
 echo "Check $HOME/thesis-project/sample-project/logs/model.log and $HOME/thesis-project/sample-project/logs/embedding.log for output."
